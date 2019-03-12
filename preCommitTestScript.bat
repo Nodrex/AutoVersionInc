687073a9a1dba1aa2cf32026201dc67bcc1f8196
Nodar Tchumbadze <nchumbadze@gmail.com>
2019-03-12 23:39:16 +0400

    changed full path value

--- preCommitTestScript.bat
+++ preCommitTestScript.bat
@@ -1,5 +1,5 @@
 #!/bin/sh
 #pre_commit_git_hook_which_calls_java_AutoVersionInc.jar_to_pars_buil.gradle_and_increas_versions_automatically
 #to_call_jar_file_we_need_full_path_and_for_build_gralde_also_need_full_path
-java -jar F:\netbeansPorjects\AutoVersionInc\someInnerProjectTest\AutoVersionInc.jar F:\netbeansPorjects\AutoVersionInc\someInnerProjectTest\build.gradle versionBuild aztelekomVersionCode
+java -jar F:\\netbeansPorjects\\AutoVersionInc\\someInnerProjectTest\\AutoVersionInc.jar F:\\netbeansPorjects\\AutoVersionInc\\someInnerProjectTest\\build.gradle versionBuild aztelekomVersionCode
 exit 0
