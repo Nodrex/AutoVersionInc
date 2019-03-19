ac9d800af7e8d0177322a6ca7e75272010cdc934
Nodar Tchumbadze <nchumbadze@gmail.com>
2019-03-20 00:35:47 +0400

    mini code changes

--- AutoVersionIncJava/src/com/nodrex/autoversioninc/AutoVersionInc.java
+++ AutoVersionIncJava/src/com/nodrex/autoversioninc/AutoVersionInc.java
@@ -19,7 +19,9 @@ public class AutoVersionInc {
     public static final String EMPTY_STRING = "";
 
     public static void main(String args[]) {
-       
+        //args = new String[]{"F:\\netbeansPorjects\\AutoVersionInc\\someInnerProjectTest\\build.gradle", "versionBuild", "aztelekomVersionCode"};
+        //abouv code is for testing purposes only and shoudl be commented on prod version
+        
         /*
         try{
             Git git = Git.open(new File("F:\\netbeansPorjects\\AutoVersionInc"));
